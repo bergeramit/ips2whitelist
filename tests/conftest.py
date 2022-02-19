b66afa9d4a3964f6a8c406549786fe3250da3ddc
Amit Berger <bergeramit@gmail.com>
2022-02-19 19:42:39 +0200

    Added real test for whiterules

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import time
+from subprocess import Popen, PIPE
+
 from pytest import fixture
 
 def pytest_addoption(parser):
@@ -16,3 +19,12 @@ def whitelist(request):
     with open(WHITELIST_PER_ENV[request.config.getoption('--env')], "rb") as f:
         rules = [line.strip() for line in f.readlines()]
     return rules
+
+
+@fixture(scope='session')
+def run_pcap_with_rule():
+    def _run_pcap_with_rule(pcap_path, whitelist_rule):
+        pipe = Popen(['tcpdump', '-pnnvvr', f'{pcap_path}', f'{whitelist_rule}'], stdout=PIPE)
+        time.sleep(1)
+        return pipe.communicate()[0]
+    return _run_pcap_with_rule
--- tests/test_dns/test_dns_pcap.py
+++ tests/test_dns/test_dns_pcap.py
@@ -3,8 +3,8 @@ from pytest import mark
 @mark.parametrize('pcap_path', [
     ('simple_query_non_zero_Z.pcap'),
 ])
-def test_whitelist_on_pcap(whitelist, pcap_path):
+def test_whitelist_on_pcap(whitelist, pcap_path, run_pcap_with_rule):
     print(whitelist)
     print(pcap_path)
-    assert True
+    assert all([run_pcap_with_rule(pcap_path, rule) for rule in whitelist])
 
