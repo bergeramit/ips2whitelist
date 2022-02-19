a81c4f4c70a8d9aa2e9f5aeedf58682930d169cf
Amit Berger <bergeramit@gmail.com>
2022-02-19 21:21:05 +0200

    Fixed tests running on dns

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,20 +12,20 @@ def pytest_addoption(parser):
     )
 
 @fixture(scope='session')
-def whitelist(request):
+def run_pcap_with_rule():
+    def _run_pcap_with_rule(pcap_path, rule):
+        string_rule = rule.decode('utf-8')
+        pipe = Popen(['tcpdump', '-pnnvvr', f'{pcap_path}', f'{string_rule}'], stdout=PIPE)
+        time.sleep(1)
+        return pipe.communicate()[0]
+    return _run_pcap_with_rule
+
+def pytest_generate_tests(metafunc):
     WHITELIST_PER_ENV = {
         'tcp': os.path.join('examples','tcp_whitelist.txt'),
         'dns': os.path.join('examples','dns_whitelist.txt'),
     }
-    with open(WHITELIST_PER_ENV[request.config.getoption('--env')], "rb") as f:
+    with open(WHITELIST_PER_ENV[metafunc.config.getoption('--env')], "rb") as f:
         rules = [line.strip() for line in f.readlines()]
-    return rules
 
-
-@fixture(scope='session')
-def run_pcap_with_rule():
-    def _run_pcap_with_rule(pcap_path, whitelist_rule):
-        pipe = Popen(['tcpdump', '-pnnvvr', f'{pcap_path}', f'{whitelist_rule}'], stdout=PIPE)
-        time.sleep(1)
-        return pipe.communicate()[0]
-    return _run_pcap_with_rule
+    metafunc.parametrize("whitelist_rule", rules)
--- tests/test_dns/test_dns_pcap.py
+++ tests/test_dns/test_dns_pcap.py
@@ -4,8 +4,6 @@ from pytest import mark
 @mark.parametrize('pcap_path', [
     (os.path.join('tests','test_dns','simple_query_non_zero_Z.pcap')),
 ])
-def test_whitelist_on_pcap(whitelist, pcap_path, run_pcap_with_rule):
-    print(whitelist)
-    print(pcap_path)
-    assert all([run_pcap_with_rule(pcap_path, rule) for rule in whitelist])
+def test_whitelist_rule_on_pcap(whitelist_rule, pcap_path, run_pcap_with_rule):
+    assert len(run_pcap_with_rule(pcap_path, whitelist_rule)) > 0
 
